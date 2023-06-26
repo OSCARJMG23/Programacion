55adacb251c9e6d9c06fb45800fcae8de8f3309f
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-06-26 12:42:23 -0500

    se agrega cambios y new carpeta calculadora

---
+++ Calculadora-clases/script.js
@@ -0,0 +1,52 @@
+class Calculator {
+    constructor() {
+      this.pantalla = document.querySelector('.pantalla');
+      this.botones = document.querySelectorAll('.btn');
+      this.addEventListeners();
+    }
+  
+    addEventListeners() {
+      this.botones.forEach(boton => {
+        boton.addEventListener('click', () => {
+          this.handleButtonClick(boton);
+        });
+      });
+    }
+  
+    handleButtonClick(boton) {
+      const botonApretado = boton.textContent;
+  
+      if (boton.id === 'C') {
+        this.pantalla.textContent = '0';
+        return;
+      }
+  
+      if (boton.id === 'borrar') {
+        if (this.pantalla.textContent.length === 1 || this.pantalla.textContent === 'Error!') {
+          this.pantalla.textContent = '0';
+        } else {
+          this.pantalla.textContent = this.pantalla.textContent.slice(0, -1);
+        }
+        return;
+      }
+  
+      if (boton.id === 'igual') {
+        try {
+          this.pantalla.textContent = eval(this.pantalla.textContent);
+        } catch {
+          this.pantalla.textContent = 'Error!';
+        }
+        return;
+      }
+  
+      if (this.pantalla.textContent === '0' || this.pantalla.textContent === 'Error!') {
+        this.pantalla.textContent = botonApretado;
+      } else {
+        this.pantalla.textContent += botonApretado;
+      }
+    }
+  }
+  
+  // Uso de la calculadora
+  const calculator = new Calculator();
+  
