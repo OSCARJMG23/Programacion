306215cd5ff3ae1985fc4c4383749359027591a8
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-06-30 12:27:08 -0500

    Se agregan Cambios

--- local_storage/ejer_carro/script.js
+++ local_storage/ejer_carro/script.js
@@ -9,9 +9,19 @@ class Carro {
   
       for (var i = 0; i < this.cartItems.length; i++) {
         var cartItem = document.createElement('li');
+        cartItem.className = 'itemcarro'
         cartItem.textContent = this.cartItems[i].nombre + ' - $' + this.cartItems[i].precio;
+        
+        var deleteButton = document.createElement('button');
+        deleteButton.className = 'btn-elim'
+        deleteButton.textContent = 'Eliminar';
+        deleteButton.dataset.index = i; // Guardar el índice del elemento en el atributo "data-index" del botón
+        deleteButton.addEventListener('click', this.eliminarDelCarrito.bind(this)); // Agregar un evento click al botón
+        cartItem.appendChild(deleteButton);
         cartItemsList.appendChild(cartItem);
+        
       }
+      this.mostrarTotal()
     }
   
     agregarAlCarrito(item) {
@@ -26,6 +36,21 @@ class Carro {
       this.cartItems = [];
       this.mostrarCarrito();
       localStorage.removeItem('cartItems');
+    }
+    mostrarTotal() {
+      var total = 0;
+      for (var i = 0; i < this.cartItems.length; i++) {
+        total += parseFloat(this.cartItems[i].precio);
+      }
+  
+      var totalElement = document.getElementById('total');
+      totalElement.textContent = 'Total: $' + total.toFixed(2);
+    }
+    eliminarDelCarrito(event) {
+      var index = event.target.dataset.index;
+      this.cartItems.splice(index, 1);
+      this.mostrarCarrito();
+      localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
     }
   }
   
@@ -52,4 +77,10 @@ carrito.mostrarCarrito()
 const botonVaciar = document.getElementById('btn-vaciar')
 botonVaciar.addEventListener('click',function(){
     carrito.vaciarCarrito()
+})
+
+const btnCompra = document.getElementById('compra')
+
+btnCompra.addEventListener('click', function(){
+  swal("Compra Exitosa!", "Tu compra a sido aceptada!", "success");
 })
