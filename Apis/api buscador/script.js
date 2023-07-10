2f6dad295ab66db08e969e3f75adddb22219e31c
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-07-10 12:35:07 -0500

    se agregan nuevos elementos

--- Apis/api buscador/script.js
+++ Apis/api buscador/script.js
@@ -11,26 +11,35 @@ async function checkWeather(city){
     
     console.log(data)
 
-    document.querySelector('.city').innerHTML = data.name
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) +'ºc'
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
-    document.querySelector('.wind').innerHTML = data.wind.speed + ' Km/h'
+    if(response.status == 404){
+        /* alert('No existe esa ciudad') */
+        document.querySelector('.error').style.display = 'block'
+        document.querySelector('.weather').style.display = 'none'
+    }else{
 
-    if(data.weather[0].main == 'Clouds'){
-        weatherIcon.src= 'images/clouds.png'
-    }else if(data.weather[0].main == 'Clear'){
-        weatherIcon.src= 'images/clear.png'
-    }else if(data.weather[0].main == 'Rain'){
-        weatherIcon.src= 'images/rain.png'
-    }else if(data.weather[0].main == 'Drizzle'){
-        weatherIcon.src= 'images/drizzle.png'
-    }else if(data.weather[0].main == 'Mist'){
-        weatherIcon.src= 'images/mist.png'
+        document.querySelector('.city').innerHTML = data.name
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) +'ºc'
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+        document.querySelector('.wind').innerHTML = data.wind.speed + ' Km/h'
+    
+        if(data.weather[0].main == 'Clouds'){
+            weatherIcon.src= 'images/clouds.png'
+        }else if(data.weather[0].main == 'Clear'){
+            weatherIcon.src= 'images/clear.png'
+        }else if(data.weather[0].main == 'Rain'){
+            weatherIcon.src= 'images/rain.png'
+        }else if(data.weather[0].main == 'Drizzle'){
+            weatherIcon.src= 'images/drizzle.png'
+        }else if(data.weather[0].main == 'Mist'){
+            weatherIcon.src= 'images/mist.png'
+        }
+        document.querySelector('.error').style.display = 'none'
+        document.querySelector('.weather').style.display = 'block'
     }
+
 }
 
 searchBtn.addEventListener('click', ()=>{
     checkWeather(searchBox.value)
 })
 
-//https://www.youtube.com/watch?v=MIYQR-Ybrn4
---
+++ app/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string libro = "el programador pragmatico";
+
+/* int entero = 100;
+float decimal = 1.967857F; */
+
+bool autorizado = true;
+bool seleccionado = false;
+
+//array
+int[] numeros = {23, 45, 16, 37, 3, 99, 22};
+Console.WriteLine(numeros[0]); 
+
+//lista
+List<int> numeross = new List<int> {23, 45, 16, 37, 3, 99, 22};
+Console.WriteLine(numeross[0]); 
+//imprimir toda la lista
+Console.WriteLine(string.Join(",",numeross)); 
