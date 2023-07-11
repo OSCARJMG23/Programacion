fea512e292538c83d3d09ed439b11e22ea32a5e9
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-07-11 12:39:29 -0500

    Se agregan modificaciones al pre-simulacro

--- Simulacro preFiltro2/js/clases/App.js
+++ Simulacro preFiltro2/js/clases/App.js
@@ -1,2 +1,17 @@
 import { selector,contenedor,modal,limpiarCarrito } from "../selectores.js";
-import {  }
+import { selectCategoria, getCategoria,agregarCarrito,eliminarUno,vaciarCarrito } from "../funciones.js";
+
+class App{
+    constructor(){
+        this.initApp()
+    }
+    initApp(){
+        selectCategoria()
+        selector.addEventListener('input', getCategoria)
+        contenedor.addEventListener('click', agregarCarrito)
+        modal.addEventListener('click', eliminarUno)
+        limpiarCarrito.addEventListener('click', vaciarCarrito)
+    }
+}
+
+export default App;
--- Simulacro preFiltro2/js/funciones.js
+++ Simulacro preFiltro2/js/funciones.js
@@ -2,7 +2,7 @@ import { selector,contenedor,carritoCompras } from "./selectores.js";
 
 
 export function selectCategoria(){
-    const url = "www.themealdb.com/api/json/v1/1/list .php?c=list"
+    const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
     fetch(url)
         .then(resultado=>{
             return resultado.json()
@@ -35,7 +35,7 @@ export function getCategoria(e){
 function verCategoria(){
     const { categoria } = parametros
     console.log(categoria)
-    const url = `www.themealdb.com/api/json/v1/1/list .php?c=${categoria}`
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
     fetch(url)
         .then(result =>{
             return result.json()
@@ -55,12 +55,11 @@ function showComida(datosComida){
         const {strMeal, strMealThumb, idMeal } = dato
 
         html += `
-        <div class="card" style="width:28rem;">
-            <img src="${strMealThumb}" class="card-img-top" alt="...">
-            <div>
+        <div class="card">
+            <div class="card_landing" style="--i:url(${strMealThumb})">
                 <h5 class="card-title">${strMeal}</h5>
-                <a href="#" class="btn btn-primary boton" id='${idMeal}' imagen='${strMealThumb}' nombre='${strMeal}'>Añadir al carrito</a>
-            </div>
+                <a href="#" class="btn  boton" id='${idMeal}' imagen='${strMealThumb}' nombre='${strMeal}'>Añadir al carrito</a>
+            </div>          
         </div>
         `
         contenedor.innerHTML=`${html}`
@@ -71,4 +70,72 @@ function limpiar(){
     let m = document.querySelectorAll('.card')
     for(let n =0; n<m.length; n++)
     m[n].remove()
+}
+
+let arrayComida = []
+
+export function agregarCarrito(e){
+    e.preventDefault()
+    if(e.target.classList.contains('boton')){
+        const nombre = e.target.getAttribute('nombre')
+        const imagen = e.target.getAttribute('imagen')
+        const idUnico = e.target.getAttribute('id')
+
+        let comida = {
+            id: idUnico,
+            imagen: imagen,
+            nombre: nombre,
+        }
+        arrayComida=[...arrayComida,comida]
+        console.log(arrayComida)
+        injectarComida()
+    }
+}
+
+function injectarComida(){
+    limpiarHtml()
+    let html = ''
+    arrayComida.forEach((array)=>{
+        const { imagen, nombre, id} = array
+        html +=`
+        <tr>
+            <td><img src="${imagen}" style="width: 10rem;"></td>
+            <td>${nombre}</td>
+            <td>
+                <a id="${id}" class="btn btn-danger eliminar">X</a>
+            </td>
+        </tr>
+        `
+        carritoCompras.innerHTML=`${html}`
+    })
+    addStorage()
+}
+
+function addStorage(){
+    localStorage.setItem('comida', JSON.stringify(arrayComida))
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    arrayComida= JSON.parse(localStorage.getItem('comida')) || [];
+    injectarComida()
+})
+
+function limpiarHtml(){
+    carritoCompras.innerHTML=''
+}
+
+export function eliminarUno(e){
+    if(e.target.classList.contains("eliminar")){
+        const tarjetaEliminar = e.target.getAttribute("id")
+        console.log(tarjetaEliminar)
+        arrayComida=arrayComida.filter((te)=>te.id !== tarjetaEliminar)
+        console.log(arrayComida)
+        injectarComida()
+    }
+}
+
+export function vaciarCarrito(){
+    arrayComida=[]
+    localStorage.setItem('comida',JSON.stringify(arrayComida))
+    limpiarHtml()
 }
