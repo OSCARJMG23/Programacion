8eecedfa46f9402ede7e6dbbab807aad3affee66
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-07-19 15:29:30 -0500

    modificaciones

---
+++ app/ejer-1/ejer-1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// using System;
+class Programa {
+    static void Main() {
+        Console.WriteLine("Calculadora básica");
+        Console.Write("Ingresa el primer número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Suma: {num1 + num2}");
+        Console.WriteLine($"Resta: {num1 - num2}");
+        Console.WriteLine($"Multiplicación: {num1 * num2}");
+        Console.WriteLine($"División: {num1 / num2}");
+    }
+}
